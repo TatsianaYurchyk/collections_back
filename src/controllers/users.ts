4055eff1bfc5b77c194d02f9663ec757ff2e6d59
TatsianaYurchik <t26yurchik@gmail.com>
2023-02-01 00:03:38 +0300

    add signup

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,55 @@
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
 import UserModel from "../models/user";
+import bcrypt from "bcrypt";
+
+interface SignUpBody {
+    username?: string,
+    email?: string,
+    password?: string,
+}
+
+export const signUp: RequestHandler <unknown, unknown, SignUpBody, unknown> = async (req, res, next) =>{
+    const username = req.body.username;
+    const email = req.body.email;
+    const passwordRaw = req.body.password;
+    
+    try {
+        if (!username || !email || !passwordRaw) {
+            throw createHttpError(400, "All parameters are required for registration");
+        }
+
+        const existingUsername = await UserModel.findOne({username:username}).exec();
+        if (existingUsername){
+            throw createHttpError (409,"This username is already taken. Please, log in or choose another one for registration.");
+        }
+
+        const existingEmail = await UserModel.findOne({email:email}).exec();
+        if (existingEmail){
+            throw createHttpError (409,"A user with this email already exists. Please, log in.");
+        }
+
+        const passwordHashed =  await bcrypt.hash(passwordRaw, 10);
+
+        const newUser = await UserModel.create({
+            username:username,
+            email:email,
+            password:passwordHashed,
+        });
+        
+        req.session.userId = newUser._id;
+        res.status(201).json(newUser);
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+
+
+
 
 export const getUsers: RequestHandler = async (req, res, next) => {
     try {
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,7 @@ import * as UserController from "../controllers/users";
 
 const router = express.Router();
 
+router.post("/signup", UserController.signUp);
 router.get("/", UserController.getUsers);
 
 export default router;
