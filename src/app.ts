b5fcb79d89a2c870dd7aff37b6676644493e4e76
TatsianaYurchik <t26yurchik@gmail.com>
2023-01-31 23:13:00 +0300

    initial

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import express, { Request, Response, NextFunction } from "express";
+import usersRoutes from "./routes/users";
+import morgan from "morgan";
+import createHttpError, {isHttpError} from "http-errors";
+
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/users", usersRoutes)
+
+app.use((req, res, next)=>{
+    next(createHttpError(404,"Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error:unknown, req:Request, res: Response, next: NextFunction)=>{
+    console.error(error);
+    let errorMessage = "An unknown error occured";
+    let statusCode =500;
+    if (isHttpError(error)){
+        statusCode= error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({error:errorMessage});
+});    
+
+
+
+export default app;
