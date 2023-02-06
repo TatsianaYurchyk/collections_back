7432795483e89c62913b3cc53ea285b5ed459513
TatsianaYurchik <t26yurchik@gmail.com>
2023-02-06 14:01:58 +0300

    add header credentials

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import createHttpError from "http-errors";
 
 export const requiresAuth: RequestHandler = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
     if (req.session.userId) {
         next();
     } else {
