9fecc586547422455d1e078474a7b8cd35e43b5f
TatsianaYurchik <t26yurchik@gmail.com>
2023-02-07 19:40:19 +0300

    add adminpanel route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -10,10 +10,10 @@ router.get("/", requiresAuth, UserController.getAuthenticatedUser);
 router.post("/signup", UserController.signUp);
 router.post("/login",UserController.logIn);
 // router.get("/get", requiresAdm ,UserController.getUsers);
-router.get("/get", UserController.getUsers);
-router.delete("/:userId",UserController.deleteUser);
-router.patch("/:userId", UserController.updateUserStatus);
-router.patch("/togglerole/:userId", UserController.updateUserRole);
+router.get("/admin/get", UserController.getUsers);
+router.delete("/admin/:userId",UserController.deleteUser);
+router.patch("/admin/:userId", UserController.updateUserStatus);
+router.patch("/admin/togglerole/:userId", UserController.updateUserRole);
 router.post("/logout", UserController.logOut);
 
 export default router;
